f6e06053acd4302c607de9eb82879cfc8e485dba
“Carolina <carolinanichita89@gmail.com>
2023-07-14 16:17:32 +0200

    update cors

--- src/main/java/javapuffs/journeo/CorsConfiguration.java
+++ src/main/java/javapuffs/journeo/CorsConfiguration.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class CorsConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**")
+        registry.addMapping("/**")
                 .allowedMethods("GET", "POST", "PUT", "DELETE");
     }
 }
