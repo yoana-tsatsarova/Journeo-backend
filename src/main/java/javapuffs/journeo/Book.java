596d653a7dc0689f405c833ca7c5bacb5a77be68
yoana-tsatsarova <yoana.tsatsarovau@appliedtechnology.se>
2023-07-14 10:38:37 +0200

    Add github actions

--- src/main/java/javapuffs/journeo/Book.java
+++ src/main/java/javapuffs/journeo/Book.java
@@ -1,2 +1,11 @@
-package javapuffs.journeo;public class Book {
+package javapuffs.journeo;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="books")
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 }
